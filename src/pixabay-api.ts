7bb166142617eaa9efa439ffd7283d91c30cc111
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-01-17 09:43:08 +0100

    Update pixabay-api.ts

--- src/pixabay-api.ts
+++ src/pixabay-api.ts
@@ -1,23 +1,25 @@
 import axios from "axios";
+import type { PixabayResponse } from "./types/pixabay";
 
-export interface Image {
-  id: number;
-  webformatURL: string;
-  largeImageURL: string;
-  tags: string;
-}
-
-export interface ApiResponse {
-  hits: Image[];
-  totalHits: number;
-}
+// Твой ключ API — желательно хранить в .env, но можно пока так
+const API_KEY = "ВАШ_API_KEY";
+const BASE_URL = "https://pixabay.com/api/";
 
 export async function getImagesByQuery(
   query: string,
   page: number
-): Promise<ApiResponse> {
-  const response = await axios.get<ApiResponse>(
-    `https://pixabay.com/api/?q=${query}&page=${page}`
-  );
+): Promise<PixabayResponse> {
+  const response = await axios.get<PixabayResponse>(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+      page,
+      per_page: 20,
+    },
+  });
+
   return response.data;
-};
+}
