679c749f475159a6252830d78f0f32451f2eab0a
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-01-17 09:13:17 +0100

    Define SimpleLightbox module and options interface

--- src/types/simplelightbox.d.ts
+++ src/types/simplelightbox.d.ts
@@ -1,1 +1,18 @@
-declare module "simplelightbox" {}
+declare module 'simplelightbox' {
+  interface SimpleLightboxOptions {
+    captions?: boolean;
+    captionsData?: string;
+    captionDelay?: number;
+    close?: boolean;
+    nav?: boolean;
+    loop?: boolean;
+  }
+
+  export default class SimpleLightbox {
+    constructor(selector: string, options?: SimpleLightboxOptions);
+    refresh(): void;
+    open(): void;
+    close(): void;
+    destroy(): void;
+  }
+}
